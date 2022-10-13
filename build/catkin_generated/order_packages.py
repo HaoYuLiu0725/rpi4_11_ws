15cbdc734909424d0d792d52e87466027db7aa9d
HaoYuLiu0725 <lhjh0212531@gmail.com>
2022-10-14 00:26:36 +0800

    initial commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/haoyu/rpi4_11_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/haoyu/ros_test/devel;/opt/ros/noetic'.split(';') if '/home/haoyu/ros_test/devel;/opt/ros/noetic' != '' else []
