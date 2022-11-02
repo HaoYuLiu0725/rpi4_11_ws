c6271d4c03e38c431c1c389321fe702b1b69006b
DITShark <92559495+DITShark@users.noreply.github.com>
2022-11-03 01:59:50 +0800

    Add files via upload

--- src/main_program/src/main_tel2022.cpp
+++ src/main_program/src/main_tel2022.cpp
@@ -150,7 +150,7 @@ void checkStateMachine(ros::Publisher pub1, ros::Publisher pub2)
     {
         if (checkPosition(state_list[i].getPosition('x'), state_list[i].getPosition('y')))
         {
-            cout << "Index : " << i << endl;
+            cout << position_x << " , " << position_y << " - Index : " << i << endl;
             if (state_list[i].getCondition() == 11)
             {
                 if (moving && doing)
