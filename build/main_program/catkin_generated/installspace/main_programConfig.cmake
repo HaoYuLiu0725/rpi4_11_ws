5479ece8c21d1154838555f2d74a979ffe86ebe3
HaoYuLiu0725 <lhjh0212531@gmail.com>
2022-11-12 00:43:27 +0800

    catkin_make

--- build/main_program/catkin_generated/installspace/main_programConfig.cmake
+++ build/main_program/catkin_generated/installspace/main_programConfig.cmake
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/haoyu/rpi4_11_ws/install/lib;/home/haoyu/ros_test/devel/lib;/opt/ros/noetic/lib)
+    foreach(path /home/haoyu/rpi4_11_ws/install/lib;/home/haoyu/rpi4_11_ws/devel/lib;/home/haoyu/ros_test/devel/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
