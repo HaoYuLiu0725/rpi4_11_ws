1532f5139791837c3a708c9b7e2f0912ea2fe388
HaoYuLiu0725 <lhjh0212531@gmail.com>
2022-11-19 17:12:18 +0800

    Update start_caller

--- src/main_program/src/start_caller.cpp
+++ src/main_program/src/start_caller.cpp
@@ -1,24 +1,29 @@
 #include <ros/ros.h>
-#include "wiringPi.h"
 #include <std_srvs/Empty.h>
 #include <std_msgs/Int32.h>
-​
+
+#ifdef __arm__
+#include <wiringPi.h>
+#endif
+
 #define gpio_pin 26
-​
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "start_caller");
     ros::NodeHandle nh;
+
+#ifdef __arm__
     wiringPiSetupGpio();
     pullUpDnControl(gpio_pin, PUD_UP);
-​
+
     // ros::ServiceClient start_client = nh.serviceClient<std_srvs::Empty>("/startRunning");
     ros::Publisher start_pub = nh.advertise<std_msgs::Int32>("/start_running", 10);
     std_srvs::Empty srv_;
     std_msgs::Int32 topic_;
     int gpio_state = -1; // read gpio state
     int gpio_state_past = -1;
-​
+
     while (ros::ok())
     {
         gpio_state_past = gpio_state;
@@ -41,4 +46,7 @@ int main(int argc, char **argv)
 ​
         ros::spinOnce();
     }
+
+#endif
+
 }
