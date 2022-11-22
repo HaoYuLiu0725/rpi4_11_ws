ae742883561247ca4cd2a45131e1ffd7a8d85737
HaoYuLiu0725 <lhjh0212531@gmail.com>
2022-11-23 02:04:43 +0800

    Update arm_move

--- src/arm_move/src/arm_move.cpp
+++ src/arm_move/src/arm_move.cpp
@@ -159,7 +159,7 @@ void ArmMove::armStatusCallback(const std_msgs::Bool::ConstPtr& ptr)
 void ArmMove::suckStatusCallback(const std_msgs::Bool::ConstPtr& ptr)  
 {
     suck_status = *ptr;
-    ROS_INFO_STREAM("[Arm Move]: suck status : " << suck_status.data);
+    ROS_INFO_STREAM("[Arm Move]: suck status : " << suck_status);
 }
 
 void ArmMove::timerCallback(const ros::TimerEvent& e)
