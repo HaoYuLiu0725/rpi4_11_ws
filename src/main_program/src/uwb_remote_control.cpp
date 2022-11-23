3797132cde14192d03a0980926e5ee0b6a0dd11e
HaoYuLiu0725 <lhjh0212531@gmail.com>
2022-11-24 00:03:14 +0800

    Update uwb_remote_control.cpp

--- src/main_program/src/uwb_remote_control.cpp
+++ src/main_program/src/uwb_remote_control.cpp
@@ -14,7 +14,7 @@ class RemoteControl
 public:
     RemoteControl(ros::NodeHandle& nh, ros::NodeHandle& nh_local) : nh_(nh), nh_local_(nh_local)
     {
-        timer_ = nh_.createTimer(ros::Duration(1.0), timerCallback, this, false, false);
+        timer_ = nh_.createTimer(ros::Duration(1.0), &RemoteControl::timerCallback, this, false, false);
         initialize();
     }
 
@@ -39,15 +39,17 @@ private:
     ros::Time last_time_;
     ros::Duration timeout_;
 
+    geometry_msgs::Point init_arm;
+    geometry_msgs::Point storage_1;
+    geometry_msgs::Point storage_2;
+    geometry_msgs::Point putSquare;
+
     /* ros param */
     bool p_active_;
 
     double p_frequency_;
     double p_MAX_linear_speed_;     // [m/s]
     double p_MAX_angular_speed_;    // [rad/s]
-    double p_init_arm_x;            // [mm]
-    double p_init_arm_y;            // [mm]
-    double p_init_arm_z;            // [mm]
     double p_arm_MAX_XYspeed_;      // [mm/s]
     double p_arm_MAX_Zspeed_;       // [mm/s]
     double p_X_max_;                // [mm]
@@ -56,6 +58,18 @@ private:
     double p_Y_min_;                // [mm]
     double p_Z_max_;                // [mm]
     double p_Z_min_;                // [mm]
+    double p_init_arm_x;
+    double p_init_arm_y;
+    double p_init_arm_z;
+    double p_storage1_x;
+    double p_storage1_y;
+    double p_storage1_z;
+    double p_storage2_x;
+    double p_storage2_y;
+    double p_storage2_z;
+    double p_putSquare_x;
+    double p_putSquare_y;
+    double p_putSquare_z;
 
     std::string p_joy_topic_;
     std::string p_twist_topic_;
@@ -79,9 +93,6 @@ private:
         get_param_ok = nh_local_.param<double>("frequency", p_frequency_, 50);
         get_param_ok = nh_local_.param<double>("MAX_linear_speed", p_MAX_linear_speed_, 0.8);   // [m/s]
         get_param_ok = nh_local_.param<double>("MAX_angular_speed", p_MAX_angular_speed_, 1.0); // [rad/s]
-        get_param_ok = nh_local_.param<double>("init_arm_x", p_init_arm_x, 128.0);              // [mm]
-        get_param_ok = nh_local_.param<double>("init_arm_y", p_init_arm_y, 17.0);               // [mm]
-        get_param_ok = nh_local_.param<double>("init_arm_z", p_init_arm_z, 10.0);               // [mm]
         get_param_ok = nh_local_.param<double>("arm_MAX_XYspeed", p_arm_MAX_XYspeed_, 100.0);   // [mm/s]
         get_param_ok = nh_local_.param<double>("arm_MAX_Zspeed", p_arm_MAX_Zspeed_, 100.0);     // [mm/s]
         get_param_ok = nh_local_.param<double>("X_max", p_X_max_, 516.0);                       // [mm]
@@ -90,6 +101,18 @@ private:
         get_param_ok = nh_local_.param<double>("Y_min", p_Y_min_, -516.0);                      // [mm]
         get_param_ok = nh_local_.param<double>("Z_max", p_Z_max_, 119.0);                       // [mm]
         get_param_ok = nh_local_.param<double>("Z_min", p_Z_min_, -58.0);                       // [mm]
+        get_param_ok = nh_local_.param<double>("init_arm_x", p_init_arm_x, 128.0);              // [mm]
+        get_param_ok = nh_local_.param<double>("init_arm_y", p_init_arm_y, 17.0);               // [mm]
+        get_param_ok = nh_local_.param<double>("init_arm_z", p_init_arm_z, 10.0);               // [mm]
+        get_param_ok = nh_local_.param<double>("storage1_x", p_storage1_x, 82.0);               // [mm]
+        get_param_ok = nh_local_.param<double>("storage1_y", p_storage1_y, -99.0);              // [mm]
+        get_param_ok = nh_local_.param<double>("storage1_z", p_storage1_z, 74.0);               // [mm]
+        get_param_ok = nh_local_.param<double>("storage2_x", p_storage2_x, 82.0);               // [mm]
+        get_param_ok = nh_local_.param<double>("storage2_y", p_storage2_y, -199.0);             // [mm]
+        get_param_ok = nh_local_.param<double>("storage2_z", p_storage2_z, 74.0);               // [mm]
+        get_param_ok = nh_local_.param<double>("putSquare_x", p_putSquare_x, 0.0);              // [mm]
+        get_param_ok = nh_local_.param<double>("putSquare_y", p_putSquare_y, 200.0);            // [mm]
+        get_param_ok = nh_local_.param<double>("putSquare_z", p_putSquare_z, 50.0);            // [mm]
 
         double timeout;
         get_param_ok = nh_local_.param<double>("timeout", timeout, 0.2);
@@ -115,7 +138,7 @@ private:
         {
             if (p_active_)
             {
-                joy_sub_ = nh_.subscribe(p_joy_topic_, 10, joyCallback, this);
+                joy_sub_ = nh_.subscribe(p_joy_topic_, 10, &RemoteControl::joyCallback, this);
                 twist_pub_ = nh_.advertise<geometry_msgs::Twist>(p_twist_topic_, 10);
                 point_pub_ = nh_.advertise<geometry_msgs::Point>(p_point_topic_, 10);
                 suck_pub_ = nh_.advertise<std_msgs::Bool>(p_suck_topic_, 10);
@@ -142,9 +165,20 @@ private:
         output_twist_.linear.y = 0.0;
         output_twist_.angular.z = 0.0;
 
-        output_point_.x = p_init_arm_x;
-        output_point_.y = p_init_arm_y;
-        output_point_.z = p_init_arm_z;
+        init_arm.x = p_init_arm_x;
+        init_arm.y = p_init_arm_y;
+        init_arm.z = p_init_arm_z;
+        output_point_ = init_arm;
+
+        storage_1.x = p_storage1_x;
+        storage_1.y = p_storage1_y;
+        storage_1.z = p_storage1_z;
+        storage_2.x = p_storage2_x;
+        storage_2.y = p_storage2_y;
+        storage_2.z = p_storage2_z;
+        putSquare.x = p_putSquare_x;
+        putSquare.y = p_putSquare_y;
+        putSquare.z = p_putSquare_z;
 
         output_suck_.data = false;
 
@@ -203,8 +237,12 @@ private:
 
     void updateBool()
     {
-        if(input_joy_.buttons[1]) output_suck_.data = true; // button circle
-        if(input_joy_.buttons[0]) output_suck_.data = false; // button cross
+        if(input_joy_.buttons[1]) output_suck_.data = true;     // button circle
+        if(input_joy_.buttons[0]) output_suck_.data = false;    // button cross
+        if(input_joy_.buttons[12]) output_point_ = init_arm;    // button options
+        if(input_joy_.buttons[5]) output_point_ = storage_1;    // rear right 1 (R1)
+        if(input_joy_.buttons[4]) output_point_ = storage_2;    // rear left 1 (L1)
+        if(input_joy_.buttons[3]) output_point_ = putSquare;    // button square
     }
 
     void publish()
