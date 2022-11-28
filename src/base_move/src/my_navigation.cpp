3c0c0d9357397cbbef3629124777cc61e2e674ab
HaoYuLiu0725 <lhjh0212531@gmail.com>
2022-11-28 17:01:37 +0800

    Update my_navigation

--- src/base_move/src/my_navigation.cpp
+++ src/base_move/src/my_navigation.cpp
@@ -229,7 +229,7 @@ void My_navigation::stopTurn()
     if (hasStopped()){
         ROS_INFO_STREAM("[Reached goal_Theta !]");
         print_once = true;
-        move_state = ALL_STOP;
+        move_state = LINEAR;
     }
     else{
         twistPublish(0, 0, 0);
@@ -260,7 +260,6 @@ void My_navigation::moveTimerCallback(const ros::TimerEvent& e)
         else if (move_state == STOP_TURN){
             stopTurn();
         }
-        else twistPublish(0, 0, 0);
     }
     else{
         twistPublish(0, 0, 0);
