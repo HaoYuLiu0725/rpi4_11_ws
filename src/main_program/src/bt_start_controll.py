e89bb3b22f09f4e6c8e019aac8b4703e06dcc78c
HaoYuLiu0725 <lhjh0212531@gmail.com>
2022-12-18 01:39:19 +0800

    Update bt_start_controll.py

--- src/main_program/src/bt_start_controll.py
+++ src/main_program/src/bt_start_controll.py
@@ -3,34 +3,48 @@
 
 import rospy
 import serial
-# from urllib import response
-# from main_program.srv import *
+import copy
+from urllib import response
+from main_program.srv import *
 
 port = serial.Serial("/dev/rfcomm0", baudrate=9600)
 
 def main():
-	port.reset_input_buffer()
-	# rospy.init_node("py_bt_start_controll")
-	# rospy.wait_for_service("/startRunning")
+	port.reset_inVar_buffer()
+	rospy.init_node("py_bt_start_controll")
+	running = False
+	inVar = 0
+	last_inVar = 0
+	req = starting
 	script = 0
-	# request = starting
-	# request._request_class.startStatus = script
-	# rate = rospy.Rate(10)
-	# while not rospy.is_shutdown():
-		# script = port.read_until().decode()
-		# print(script)
-		# print(type(script))
-		# try:
-		# 	client = rospy.ServiceProxy("name_and_age", starting)
-		# 	response = client()
-		# 	rospy.loginfo("Message from server: %s" %response.feedback)
-		# except rospy.ServiceException as e:
-		# 	rospy.logwarn("Service call failed: %s" %e)
-		# rate.sleep()
-	while True:
-		script = int(port.read_until().decode())
-		print(script)
-		# print(type(script))
+	req._request_class.startStatus = script
+	req._request_class.startTrigger = False
+
+	rospy.wait_for_service("/startRunning")
+
+	rate = rospy.Rate(10)
+	while not rospy.is_shutdown():
+		if(not running):
+			print(f"Waiting... || script = {script} || Trigger = {req._request_class.startTrigger}")
+			last_inVar = inVar
+			inVar = int(port.read_until().decode())
+			print(inVar)
+			if(inVar != last_inVar):
+				if(inVar == 99):
+					req._request_class.startTrigger = True
+				else:
+					script = inVar
+					req._request_class.startStatus = script
+
+				print(f"Changed ! || script = {script} || Trigger = {req._request_class.startTrigger}")
+				
+				try:
+					client = rospy.ServiceProxy("/startRunning", starting)
+					response = client(req)
+					# rospy.loginfo("Message from server: %s" %response.feedback)
+				except rospy.ServiceException as e:
+					rospy.logwarn("Service call failed: %s" %e)
+		rate.sleep()		
 
 if __name__ == "__main__":
 	try:
