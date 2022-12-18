3076689b71e986d7fe6c913076d352b876633e2f
HaoYuLiu0725 <lhjh0212531@gmail.com>
2022-12-18 18:27:22 +0800

    Update bt_start_controll.py

--- src/main_program/src/bt_start_controll.py
+++ src/main_program/src/bt_start_controll.py
@@ -40,7 +40,6 @@ def main():
 				
 				try:
 					client = rospy.ServiceProxy("/startRunning", starting)
-					response = client(req)
 					# rospy.loginfo("Message from server: %s" %response.feedback)
 				except rospy.ServiceException as e:
 					rospy.logwarn("Service call failed: %s" %e)
